851a3b218e8f52d6d4573eff0bdfec7fe473ed07
AlekseyChan <aleksey.chan@gmail.com>
2017-06-18 22:45:33 +1100

    initial

---
+++ mdNote/mdNote/mdNote/Views/AboutPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+using Xamarin.Forms;
+
+namespace mdNote.Views
+{
+	public partial class AboutPage : ContentPage
+	{
+		public AboutPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ mdNote/mdNote/mdNote/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿
+using mdNote.ViewModels;
+
+using Xamarin.Forms;
+
+namespace mdNote.Views
+{
+	public partial class ItemDetailPage : ContentPage
+	{
+		ItemDetailViewModel viewModel;
+
+        // Note - The Xamarin.Forms Previewer requires a default, parameterless constructor to render a page.
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+        }
+
+        public ItemDetailPage(ItemDetailViewModel viewModel)
+		{
+			InitializeComponent();
+
+			BindingContext = this.viewModel = viewModel;
+		}
+	}
+}
